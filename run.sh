99ef854881e0c1db55d8ebc4b90f16633ba7dc0e
ichn-hu <zfhu16@fudan.edu.cn>
2019-08-07 23:10:26 -0700

    update vocabulary

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ PYTHONIOENCODING=UTF-8 python vocab.py
 git add .
 git commit -m "update vocabulary"
 cid=$(git rev-parse HEAD)
-sed -i "s/const CACHE_NAME = '[a-z0-9]*'/const CACHE_NAME = '${cid}'/"
+sed -i "s/const CACHE_NAME = '[a-z0-9]*'/const CACHE_NAME = '${cid}'/" sw.js
 git add .
 git commit -m "update cache"
 git push
